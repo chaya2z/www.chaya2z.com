ed3c4f2f7800d018f4985f6500bc14ef202414f8
chaya2z <taisuke.nakano@chaya2z.com>
2024-08-11 10:20:59 +0900

    [ Update ] New PGP key fingerprint

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -8,13 +8,7 @@ function Index() {
             <p>Handle Name: chaya2z</p>
             <p>Live in: Japan</p>
             <h2 className="text-3xl py-3">PGP</h2>
-            <p>Fingerprint: FA3C 3FA3 4507 7C43 011C 9BA7 5669 B9AF 6CB6 3972</p>
-            <p>
-              <a
-                href="https://keyoxide.org/FA3C3FA345077C43011C9BA75669B9AF6CB63972"
-              >Keyoxide</a
-              >
-            </p>
+            <p>Fingerprint: A868 356B 1484 C0B7 5749  33CB A5EC 81D8 7271 51DA</p>
           </div>
         </div>
       </main>
