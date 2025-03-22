5f48a0fd9a2a26452c955f935a6f6b9d3ae58499
chaya2z <taisuke.nakano@chaya2z.com>
2025-03-23 01:16:00 +0900

    [ Add ] `@tailwindcss/typography` style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 function Header({ title }: { title: string }) {
   return (
     <header>
-      <div className="flex justify-center p-6 mb-2 bg-gray-200">
+      <div className="flex justify-center p-6 mb-8 bg-gray-200">
         <div className="container max-w-3xl">
           <h1 className="text-3xl">{title}</h1>
         </div>
