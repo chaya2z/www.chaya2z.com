cbd41e0d483eb6cd41798b9a9f1b8e85b7e2d39a
chaya2z <taisuke.nakano@chaya2z.com>
2026-05-05 20:20:43 +0900

    [ Update ] migrate eslint config to flat config (refined structure)

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default [
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ['**/*.astro'],
+    rules: {
+      'astro/no-set-html-directive': 'error',
+    }
+  },
+  eslintConfigPrettier
+];
